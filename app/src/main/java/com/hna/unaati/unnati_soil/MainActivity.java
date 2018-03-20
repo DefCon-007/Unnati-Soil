5ff07b98926388e770204118710662299f7e5b94
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-03-20 21:02:23 +0530

    Adds option to get current location and changes app theme

--- app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
+++ app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
@@ -1,18 +1,11 @@
 package com.hna.unaati.unnati_soil;
 
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -29,7 +22,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -42,7 +34,6 @@ public class MainActivity extends AppCompatActivity
 
     private MapView mapView;
     private GoogleMap gmap;
-    private CameraPosition mCameraPosition;
     private MarkerOptions mMarkerOptions;
     private  Marker mMarker;
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
@@ -51,12 +42,6 @@ public class MainActivity extends AppCompatActivity
     // not granted.
     private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
     private static final int DEFAULT_ZOOM = 15;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean mLocationPermissionGranted;
-
-    private Location mLastKnownLocation;
-
-    private LocationManager lm;
 
     // Keys for storing activity state.
     private static final String KEY_CAMERA_POSITION = "camera_position";
@@ -73,30 +58,31 @@ public class MainActivity extends AppCompatActivity
         mMarkerOptions = new MarkerOptions().position(mDefaultLocation)
                 .draggable(true)
                 .title("Default Title");
-        lm = (LocationManager)getSystemService(getApplicationContext().LOCATION_SERVICE);
+        final gpsTracker gps = new gpsTracker(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 fabClicked = (fabClicked) ? false : true ;
-                Log.d("Fab Clicked Value",String.valueOf(fabClicked));
-                getLocationPermission();
-                Log.d("LocationPermission", String.valueOf(mLocationPermissionGranted));
-                if (mLocationPermissionGranted){
-                    @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    if (location != null) {
-                        double longitude = location.getLongitude();
-                        double latitude = location.getLatitude();
-                        LatLng lastPosition = new LatLng(latitude,longitude) ;
-                        Log.d("Last Location", String.valueOf(lastPosition));
-                        updateLocation(lastPosition);
-                    }
+                gps.getLocation();
+                if (gps.canGetLocation){
+                    LatLng currentPos = new LatLng(gps.getLatitude(),gps.getLongitude());
+                    updateLocation(currentPos);
+                    Log.d("Lat" , String.valueOf(gps.getLatitude()));
+                    Log.d("long", String.valueOf(gps.getLongitude()));
                 }
                 else {
-                Snackbar.make(view, getString(R.string.gps_permission_denied), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();}
+                    //
+                }
+
+
+//
+//                Snackbar.make(view, getString(R.string.gps_permission_denied), Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
+
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -111,8 +97,6 @@ public class MainActivity extends AppCompatActivity
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
-            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
 
         mapView = findViewById(R.id.mapView);
@@ -121,8 +105,10 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    public void getAddress(double lat, double lng) {
+    public String getAddress(LatLng position) {
         //Function to get information about location from latitude and longitude
+        double lat = position.latitude ;
+        double lng = position.longitude ;
         Geocoder geocoder = new Geocoder(this.getBaseContext(), Locale.getDefault());
         try {
             List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
@@ -137,6 +123,7 @@ public class MainActivity extends AppCompatActivity
             add = add + "\n" + obj.getSubThoroughfare();
 
             Log.v("IGA", "Address" + add);
+            return obj.getLocality() + "," + obj.getSubAdminArea();
             // Toast.makeText(this, "Address=>" + add,
             // Toast.LENGTH_SHORT).show();
 
@@ -146,8 +133,8 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+        return getString(R.string.default_marker_title);
     }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -271,8 +258,7 @@ public class MainActivity extends AppCompatActivity
 
             @Override
             public void onMarkerDragEnd(Marker marker) {
-                LatLng ll = marker.getPosition();
-                getAddress(ll.latitude,ll.longitude);
+                updateLocation(marker.getPosition());
             }
         });
     }
@@ -280,40 +266,41 @@ public class MainActivity extends AppCompatActivity
     private void updateLocation(LatLng position) {
         mMarker.setPosition(position);
         gmap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        mMarker.setTitle(getAddress(position));
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-            }
-        }
-//        updateLocationUI();
-    }
-    private void getLocationPermission() {
-    /*
-     * Request location permission, so that we can get the location of the
-     * device. The result of the permission request is handled by a callback,
-     * onRequestPermissionsResult.
-     */
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           @NonNull String permissions[],
+//                                           @NonNull int[] grantResults) {
+//        mLocationPermissionGranted = false;
+//        switch (requestCode) {
+//            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    mLocationPermissionGranted = true;
+//                }
+//            }
+//        }
+////        updateLocationUI();
+//    }
+//    private void getLocationPermission() {
+//    /*
+//     * Request location permission, so that we can get the location of the
+//     * device. The result of the permission request is handled by a callback,
+//     * onRequestPermissionsResult.
+//     */
+//        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLocationPermissionGranted = true;
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//    }
 
     @Override
     public void onLocationChanged(Location location) {
