5f40eb3241c64fe8c9f6ed71a7a9a73a719f6d17
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-03-22 21:57:58 +0530

    Adds prediction class

---
+++ app/src/main/java/com/hna/unaati/unnati_soil/prediction.java
@@ -0,0 +1,25 @@
+package com.hna.unaati.unnati_soil;
+
+/**
+ * Created by defcon on 22/03/18.
+ */
+
+public class prediction {
+    private double organicCarbon, totalNitorgen, sand, pH, clay ;
+
+    public void prediction(double sand, double clay, double pH, double organicCarbon  ){
+        this.clay = clay ;
+        this.pH = pH;
+        this.organicCarbon = organicCarbon ;
+        this.sand = sand ;
+        findTotalNitrogen();
+    }
+
+    private void findTotalNitrogen(){
+        totalNitorgen =  0.026 + 0.067*organicCarbon ;
+    }
+
+    public double getTotalNitorgen(){
+        return this.totalNitorgen;
+    }
+}
