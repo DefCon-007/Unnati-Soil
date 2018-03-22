fc8e2aba49a3e6056eb5995e843df4202f5e8d41
Nishant Nikhil <i.nishantnikhil@gmail.com>
2018-03-22 22:30:35 +0530

    nearest lat lng

--- app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
+++ app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
@@ -29,9 +29,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
@@ -59,9 +64,40 @@ public class MainActivity extends AppCompatActivity
 
     private Context mContext;
 
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        char unit = 'K';
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+            dist = dist * 1.609344;
+        } else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int total_entries = 563;
+        JSONObject cities_latlng = new JSONObject();
         try {
             InputStreamReader is = new InputStreamReader(getAssets()
                     .open("data.csv"));
@@ -69,14 +105,59 @@ public class MainActivity extends AppCompatActivity
             BufferedReader reader = new BufferedReader(is);
             reader.readLine();
             String line;
+            int i =0;
             while ((line = reader.readLine()) != null) {
-                Log.d("read", line);
+                i+=1;
+                if (i == 1)
+                    continue; //to bypass header
+                String[] separated = line.split(",");
+                //Log.d("read", separated[3]);
+
+                JSONObject soil_data = new JSONObject();
+                try {
+                    soil_data.put("sand",  Double.parseDouble(separated[7]));
+                    soil_data.put("clay",  Double.parseDouble(separated[8]));
+                    soil_data.put("ph",  Double.parseDouble(separated[9]));
+                    soil_data.put("carbon",  Double.parseDouble(separated[10]));
+
+                    if(cities_latlng.has(separated[3]+","+separated[4])) {
+                        cities_latlng.getJSONObject(separated[3]+","+separated[4]).put(separated[6], soil_data); //depth - data
+                    }
+                    else {
+                        JSONObject soil_data_depth = new JSONObject();
+                        soil_data_depth.put(separated[6], soil_data);
+                        cities_latlng.put(separated[3]+","+separated[4], soil_data_depth);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
             }
+            Log.d("read", cities_latlng.toString());
         }
         catch (IOException e) {
             Log.d("read", e.toString());
 
         }
+
+        double lat1 = 22.85;
+        double lat2 = 88.65;
+        double min = 100000;
+        Iterator<String> iter = cities_latlng.keys();
+        String lat_lng_min="";
+        while (iter.hasNext()) {
+            String key = iter.next();
+            String[] lat_lng = key.split(",");
+            double dist = distance(lat1, lat2, Double.parseDouble(lat_lng[0]), Double.parseDouble(lat_lng[1]));
+            if (dist < min) {
+                min = dist;
+                lat_lng_min = key;
+            }
+        }
+
+        Log.d("read", lat_lng_min);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
