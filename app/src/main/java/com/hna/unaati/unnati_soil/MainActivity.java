014c874942e72d3c1a4f49d5f9ae93ab30e11bb6
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-03-24 00:42:55 +0530

    Adds map in result activity

--- app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
+++ app/src/main/java/com/hna/unaati/unnati_soil/MainActivity.java
@@ -55,14 +55,14 @@ public class MainActivity extends AppCompatActivity
     private GoogleMap gmap;
     private MarkerOptions mMarkerOptions;
     private  Marker mMarker;
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    public static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
     private static final String PREFS_NAME = "UnnatiPref1kdvbbvw";
     private SharedPreferences shPref ;
 
 
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
-    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private final LatLng mDefaultLocation = new LatLng(22.3218, 87.3074);
     private double lat,lang;
     private static final int DEFAULT_ZOOM = 15;
 
@@ -72,39 +72,11 @@ public class MainActivity extends AppCompatActivity
 
     private Context mContext;
 
-
-    private double distance(double lat1, double lon1, double lat2, double lon2) {
-        char unit = 'K';
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        if (unit == 'K') {
-            dist = dist * 1.609344;
-        } else if (unit == 'N') {
-            dist = dist * 0.8684;
-        }
-        return (dist);
-    }
-
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    /*::  This function converts decimal degrees to radians             :*/
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    /*::  This function converts radians to decimal degrees             :*/
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-    }
-
     private gpsTracker gps;
     private FloatingActionButton fabLocation, fabResult;
     JSONObject cities_latlng = new JSONObject();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -564,7 +536,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d("New Location", String.valueOf(location.getLatitude()));
         if (fabClicked) {
             updateLocation(new LatLng(location.getLatitude(),location.getLongitude()));
         }
@@ -619,4 +590,33 @@ public class MainActivity extends AppCompatActivity
 //        }
 //    }
 
+
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        char unit = 'K';
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+            dist = dist * 1.609344;
+        } else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
 }
--- app/src/main/java/com/hna/unaati/unnati_soil/resultNew.java
+++ app/src/main/java/com/hna/unaati/unnati_soil/resultNew.java
@@ -1,17 +1,39 @@
 package com.hna.unaati.unnati_soil;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class resultNew extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class resultNew extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback,LocationListener {
 
     private EditText ed_sand,ed_clay,ed_ph,ed_oc;
     private TextView tv_nitrogen;
+    private MapView mapView;
+    private GoogleMap gmap;
+    private MarkerOptions mMarkerOptions;
+    private  Marker mMarker;
+    private gpsTracker gps;
+    private LatLng mDefaultLocation = new LatLng(22.3218, 87.3074);
+    private int mDefaultZoom = 15;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -42,6 +64,13 @@ public class resultNew extends AppCompatActivity {
         ed_oc.setText(String.valueOf(p.organicCarbon));
         tv_nitrogen.setText(String.valueOf(p.getTotalNitorgen())+"%");
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MainActivity.MAP_VIEW_BUNDLE_KEY);
+        }
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
@@ -52,6 +81,137 @@ public class resultNew extends AppCompatActivity {
         ed_oc = (EditText) findViewById(R.id.edOC);
         ed_ph = (EditText) findViewById(R.id.edpH);
         tv_nitrogen = (TextView) findViewById(R.id.tvNitrogenContent);
+        mapView = (MapView) findViewById(R.id.mapView);
+        gps = new gpsTracker(this.getApplicationContext());
+        mMarkerOptions = new MarkerOptions().position(mDefaultLocation)
+                .draggable(false);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MainActivity.MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MainActivity.MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(mDefaultZoom);
+        mMarker = gmap.addMarker(mMarkerOptions);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(getLocation()));
+        gmap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                updateLocation(marker.getPosition());
+            }
+        });
+        gmap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Intent i = new Intent(resultNew.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+//        gmap.setClickable(false);
+        mapView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(resultNew.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void updateLocation(LatLng position) {
+        mMarker.setPosition(position);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(position));
+//        mMarker.setTitle(getAddress(position));
+    }
+
+    private LatLng getLocation(){
+        gps.getLocation();
+        if (gps.canGetLocation){
+            LatLng currentPos = new LatLng(gps.getLatitude(),gps.getLongitude());
+//            updateLocation(currentPos);
+//            lat = gps.getLatitude();
+//            lang = gps.getLongitude();
+            return currentPos;
+        }
+        return mDefaultLocation;
+    }
 }
